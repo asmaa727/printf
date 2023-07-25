d284af6ec495f0fb7bde90caa28f8455764f4135
Asmaa <engineerasmaa2019@gmail.com>
2023-07-25 08:28:48 -0700

    task0.c

---
+++ task0.c
@@ -0,0 +1,66 @@
+#include "main.h"
+/**
+* print_char - Prints a char
+* @types: List a of arguments
+* @buffer: Buffer array to handle the printing
+* @flags: Checks active flags
+* @width: the Width
+* @precision: the Precision specification
+* @size: the Size specifier
+* Return: the Number of chars printed
+*/
+int print_char(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+char c = va_arg(types, int);
+return (handle_write_char(c, buffer, flags, width, precision, size));
+}
+/**
+* print_string - Prints a string
+* @types: List a of arguments
+* @buffer: Buffer array to handle print
+* @flags: Calculates active flags
+* @width: get width.
+* @precision: Precision specification
+* @size: Size specifier
+* Return: Number of chars printed
+*/
+int print_string(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+int length = 0, i;
+char *str = va_arg(types, char *);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+if (str == NULL)
+{
+str = "(null)";
+if (precision >= 6)
+str = " ";
+}
+while (str[length] != '\0')
+length++;
+if (precision >= 0 && precision < length)
+length = precision;
+if (width > length)
+{
+if (flags & F_MINUS)
+{
+write(1, &str[0], length);
+for (i = width - length; i > 0; i--)
+write(1, " ", 1);
+return (width);
+}
+else
+{
+for (i = width - length; i > 0; i--)
+write(1, " ", 1);
+write(1, &str[0], length);
+return (width);
+}
+}
+return (write(1, str, length));
+}
